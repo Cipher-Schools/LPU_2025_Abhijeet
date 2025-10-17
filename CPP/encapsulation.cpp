884b1af70f3d2a83b5b7b1064a5546629a0d7e33
AbhijeetCipherSchools <abhijeet.kumar@cipherschools.com>
2025-10-17 11:10:45 +0530

    done

---
+++ CPP/encapsulation.cpp
@@ -0,0 +1,56 @@
+
+// Online Free C++ compiler to run C++ program online
+#include <iostream>
+using namespace std ;
+// Implementing encapsulation
+// make all data members as private and make getter setter funtions to access it .
+class BankAccount {
+private :
+string name ;
+    float amount  ;
+
+
+public :
+BankAccount(string name,  int amount){
+    this->name = name ;
+    this->amount = amount ;
+}
+    string get_name(){
+    return this->name ;
+}
+    float  get_amount(){
+    return this->amount ;
+}
+    void set_name(string name) {
+    this->name = name ;
+}
+    void set_amount(int amount){
+    this->amount = amount ;
+}
+
+    // withdraw
+    // depostite function
+
+
+
+} ;
+int main() {
+    // OOPS pillars
+
+    // 1) encapsulation
+
+    // bundling data and methods together  is encapsulation
+
+    // controlling who is able to access what in the class
+
+    BankAccount b("A" , 1000) ;
+
+    cout<<b.get_name() ;
+
+
+
+
+
+
+    return 0;
+}
